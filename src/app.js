2e6a6cbc587cf6cf5d84d1f6c1974bbec2416c79
Sandor Kunyik <postmaster@kane-il.us>
2026-07-10 12:41:46 -0500

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 (function bootKaneMap(global) {
   "use strict";
 
-  const BATCH_LABEL = "UI: Batch 087";
+  const BATCH_LABEL = "UI: Batch 088";
 
   function installControllers(ctx) {
     global.KaneMapWorkspaceController.installWorkspaceController(ctx);
